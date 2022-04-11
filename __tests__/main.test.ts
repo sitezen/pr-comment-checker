af42239b85e75bcbf7acc7329f16a01163ea4ff9
Aleksandr Kirichenko <alexander-kir@ya.ru>
2022-04-11 16:17:00 +0700

    Changed params to actual, functions and tests added

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,82 @@
-import {wait} from '../src/wait'
+import {readFileSync} from 'fs'
+import * as github from '@actions/github'
+import {isCaptionValid, isDescriptionValid} from '../src/main'
+import {getPRCaption, getPRComment} from '../src/pr-data'
 import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+describe('get PR data', () => {
+  it('gets a pull request comment', () => {
+    process.env['GITHUB_EVENT_PATH'] = __dirname + '/pr1.json'
+    github.context.payload = JSON.parse(
+      readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf8'})
+    )
+    const pr_comment = getPRComment()
+    expect(pr_comment).toEqual('Test PR comment\r\n123')
+  })
+
+  it('gets a pull request caption', () => {
+    process.env['GITHUB_EVENT_PATH'] = __dirname + '/pr1.json'
+    github.context.payload = JSON.parse(
+      readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf8'})
+    )
+    const pr_comment = getPRCaption()
+    expect(pr_comment).toEqual('Test PR')
+  })
+
+  it('throws an error if the event is not a pull_request type', () => {
+    process.env['GITHUB_EVENT_PATH'] = __dirname + '/not_pr.json'
+    github.context.payload = JSON.parse(
+      readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf8'})
+    )
+    expect(getPRComment).toThrowError(
+      'This action should only be run with Pull Request Events'
+    )
+  })
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  const delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+describe('PR checkers', () => {
+  it('Should return correct result for isCaptionValid without params passed', () => {
+    expect(isCaptionValid('Some PR caption')).toBeTruthy()
+  })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  it('returns correct result for isCaptionValid when params provided', () => {
+    process.env[`INPUT_${'pr_caption_should_contain'.toUpperCase()}`] =
+      'some value'
+    expect(isCaptionValid('Some PR caption')).toBeFalsy()
+    expect(isCaptionValid('Some PR caption containing some value')).toBeTruthy()
+    process.env[`INPUT_${'pr_caption_should_contain'.toUpperCase()}`] = ''
+    process.env[`INPUT_${'pr_caption_should_not_contain'.toUpperCase()}`] =
+      'ZZz'
+    expect(isCaptionValid('Some PR caption')).toBeTruthy()
+    expect(isCaptionValid('Some PR caption containing ZZz')).toBeFalsy()
+    process.env[`INPUT_${'pr_caption_should_contain'.toUpperCase()}`] = '123'
+    expect(isCaptionValid('123Some PR caption')).toBeTruthy()
+  })
+
+  it('Should return correct result for isDescriptionValid without params passed', () => {
+    expect(isDescriptionValid('Some PR description')).toBeTruthy()
+  })
+
+  it('returns correct result for isDescriptionValid when params provided', () => {
+    process.env[`INPUT_${'pr_description_should_contain'.toUpperCase()}`] =
+      'some value'
+    expect(isDescriptionValid('Some PR description')).toBeFalsy()
+    expect(
+      isDescriptionValid('Some PR description containing some value')
+    ).toBeTruthy()
+    process.env[`INPUT_${'pr_description_should_contain'.toUpperCase()}`] =
+      'regex:\\d{2}'
+    expect(
+      isDescriptionValid('Some PR description containing 33 (2 digits)')
+    ).toBeTruthy()
+    process.env[`INPUT_${'pr_description_should_contain'.toUpperCase()}`] = ''
+    process.env[`INPUT_${'pr_description_should_not_contain'.toUpperCase()}`] =
+      'ZZz'
+    expect(isDescriptionValid('Some PR description')).toBeTruthy()
+    expect(isDescriptionValid('Some PR description containing ZZz')).toBeFalsy()
+    process.env[`INPUT_${'pr_description_should_contain'.toUpperCase()}`] =
+      'regex:^\\d{3}'
+    expect(isDescriptionValid('123Some PR description')).toBeTruthy()
+  })
 })
--- src/pr-data.ts
+++ src/pr-data.ts
@@ -1,11 +1,26 @@
-import * as core from '@actions/core'
+// import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {WebhookPayload} from '@actions/github/lib/interfaces'
 
 export function getPRComment(): string {
-  const pull_request = github.context.payload.pull_request
-  core.debug(`Pull Request: ${JSON.stringify(github.context.payload)}`)
+  const payload: WebhookPayload = getPRPayload()
+  const pull_request = payload.pull_request
+  //  core.debug(`Pull Request: ${JSON.stringify(github.context.payload)}`)
   if (pull_request === undefined || pull_request.body === undefined) {
     throw new Error('This action should only be run with Pull Request Events')
   }
   return pull_request.body
+}
+
+export function getPRCaption(): string {
+  const payload: WebhookPayload = getPRPayload()
+  const pull_request = payload.pull_request
+  if (pull_request === undefined || pull_request.title === undefined) {
+    throw new Error('This action should only be run with Pull Request Events')
+  }
+  return pull_request.title
+}
+
+function getPRPayload(): WebhookPayload {
+  return github.context.payload
 }
